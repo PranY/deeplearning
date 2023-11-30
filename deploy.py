5d795d3a918d5db8ce39756907196cbe74059910
Pranjal Yadav <godricglow@gmail.com>
2023-11-30 18:41:56 +0530

    adding reqs

--- deploy.py
+++
@@ -1,5 +0,0 @@
-import random
-
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Final Val Accuracy: " + str(random.random()) + "\n")
-    outfile.write("Final Val Loss: " + str(random.random()) + "\n")
