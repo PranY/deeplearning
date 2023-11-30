5cfaa03cade6bd7c4b087b742a5658730680f171
Pranjal Yadav <godricglow@gmail.com>
2023-11-30 17:46:58 +0530

    Dummy-project

---
+++ train.py
@@ -0,0 +1,33 @@
+import random
+from dvclive import Live
+import yaml
+import mlflow
+
+
+with Live(save_dvc_exp=True) as live:
+    with mlflow.start_run():
+        epochs = yaml.safe_load(open('params.yaml'))['train']['epochs']
+
+        live.log_param("epochs", epochs)
+        mlflow.log_param("epochs", epochs)
+        for epoch in range(epochs):
+            train_acc = epoch + random.random()
+            train_loss = epochs - epoch -  random.random()
+            val_acc = epoch + random.random()
+            val_loss = epochs - epoch -  random.random()
+
+            live.log_metric("train/accuracy", train_acc)
+            live.log_metric("train/loss", train_loss)
+            live.log_metric("val/accuracy", val_acc)
+            live.log_metric("val/loss", val_loss)
+
+            mlflow.log_metric("train/accuracy", train_acc)
+            mlflow.log_metric("train/loss", train_loss)
+            mlflow.log_metric("val/accuracy", val_acc)
+            mlflow.log_metric("val/loss", val_loss)
+
+            live.next_step()
+
+with open("metrics.txt", "w") as outfile:
+    outfile.write("Final Val Accuracy: " + str(val_acc) + "\n")
+    outfile.write("Final Val Loss: " + str(val_loss) + "\n")
